23400e00358a82485328b7941f6e8fe41d4cfa73
Frank Pham <ff.pbphuoc@gmail.com>
2022-11-06 15:24:03 +1000

    test

--- FrankVicentProject/src/main/Main.java
+++ FrankVicentProject/src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is Frank Vicent team project");
+		system.out.println("6 nov 2022");
 	}
 
 }
