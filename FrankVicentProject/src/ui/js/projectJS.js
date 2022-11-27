d682035fb078d21e01b483d584efe3dee9bd4201
Frank Pham <ff.pbphuoc@gmail.com>
2022-11-27 17:42:47 +1000

    add page SearchResult, update SearchBar navigating to SearchResult

--- FrankVicentProject/src/ui/js/projectJS.js
+++ FrankVicentProject/src/ui/js/projectJS.js
@@ -5,3 +5,48 @@
  function changeProductImage(_src){
 	document.getElementById("imageViewer").src = _src;
 }
+
+$('#sorter').change( function(){
+	var selectedSorter = $('#sorter').val();
+});
+
+$('#categorySelect').change(function(){
+	selectedCategories.innerHTML = '';
+	$('#categorySelect :selected').each(function(index){
+		if(index != 0)
+			selectedCategories.innerHTML += ', ';
+		if(index != 0 && index % 3  == 0)
+			selectedCategories.innerHTML += '<br/>';
+		selectedCategories.innerHTML += this.innerHTML;
+	});	
+});
+
+$('#brandSelect').change(function(){
+	selectedBrands.innerHTML = '';
+	$('#brandSelect :selected').each(function(index){
+		if(index != 0)
+			selectedBrands.innerHTML += ', ';
+		if(index != 0 && index % 3  == 0)
+			selectedBrands.innerHTML += '<br/>';
+		selectedBrands.innerHTML += this.innerHTML;
+	});	
+});
+
+$('#availabilitySelect').change(function(){
+	selectedAvailabilities.innerHTML = '';
+	$('#availabilitySelect :selected').each(function(index){
+		if(index != 0)
+			selectedAvailabilities.innerHTML += ', ';
+		if(index != 0 && index % 3  == 0)
+			selectedAvailabilities.innerHTML += '<br/>';
+		selectedAvailabilities.innerHTML += this.innerHTML;
+	});	
+});
+
+//$('#searchBar').change(function(){
+//	alert('test');
+//	
+//});
+
+
+
