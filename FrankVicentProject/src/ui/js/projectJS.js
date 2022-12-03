453e4216311cb65fcb6f89a2757feed0e11790ac
Frank Pham <ff.pbphuoc@gmail.com>
2022-12-03 18:58:31 +1000

    Upload Checkout Page

--- FrankVicentProject/src/ui/js/projectJS.js
+++ FrankVicentProject/src/ui/js/projectJS.js
@@ -7,7 +7,7 @@
 }
 
 $('#sorter').change( function(){
-	var selectedSorter = $('#sorter').val();
+//	var selectedSorter = $('#sorter').val();
 });
 
 $('#categorySelect').change(function(){
@@ -42,6 +42,43 @@ $('#availabilitySelect').change(function(){
 		selectedAvailabilities.innerHTML += this.innerHTML;
 	});	
 });
+
+function loadOptions(opt_arr, select_id){	
+	$.each(opt_arr, function(){
+		jQuery('<option>',{
+			value: Object.values(this)[0],
+			html: Object.values(this)[1]
+		}).appendTo(select_id);
+	});
+}
+
+function loadCountryCode(){	
+	const country_codes = [
+		{country: "vietnam", code: "+84"},
+		{country: "us", code: "+1"}
+		];
+	loadOptions(country_codes, '#countryCodeList');
+}
+
+function loadSorter(){	
+	const sort_options = [
+		{sort_key: "pricelowtohigh", sort_value: "Price Low To High"},
+		{sort_key: "pricehightolow", sort_value: "Price High To Low"},
+		{sort_key: "nameatoz", sort_value: "Name A To Z"},
+		{sort_key: "nameztoa", sort_value: "Name Z To A"},
+		{sort_key: "oldtonew", sort_value: "Old To New"},
+		{sort_key: "newtoold", sort_value: "New To Old"},
+		];
+	loadOptions(sort_options, '#sorter');
+}
+
+$('#deliveryOptions input').change(function(){
+	if($(this).attr('id') == 'optDeliveryLB')
+		$('#guestAddress').show();
+	else if($(this).attr('id') == 'optCollectLB')
+		$('#guestAddress').hide();
+});
+
 
 //$('#searchBar').change(function(){
 //	alert('test');
